14d0b01a6f03b27e87e87bbe38b164d180a799a4
Aleem Pathammavong <Aleemp321@yahoo.com>
2020-01-21 10:13:46 -0700

    started

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import Home from "./components/Home";
+import Sidebar from "./components/Sidebar";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+    <>
+    <div className="main-home-container">
+      <div className="main-sidebar-container">
+        <Sidebar />
+      </div>
+      <div className="home-container">
+        <Home />
+      </div>
     </div>
+  </>
   );
-}
+};
 
 export default App;
